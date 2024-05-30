c59b5fbe7a74ffc4a3f6a8018487d47aac8dd3db
dericksito <162534670+dericksito@users.noreply.github.com>
2024-05-30 13:49:50 -0500

    Estudiante2 Actualizacion1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,10 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>BIENVENIDOS A MI TIENDA, SOY EL E1</Text>
+        <Text style={{backgroundColor:'blue'}}>PRODUCTO 1: PAPITAS</Text>
+        <Text style={{backgroundColor:'red'}}>PRODUCTO 2: SARITAS</Text>
+        <Text style={{backgroundColor:'purple'}}>PRODUCTO 3: DORITOS</Text>
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,7 +19,7 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'green',
       alignItems: 'center',
       justifyContent: 'center',
     },
